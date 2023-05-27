e7e5b48891f01206403570cd83dacf5ce1e5c272
N3ST3D Labs <134204495+N3ST3DLabs@users.noreply.github.com>
2023-05-28 01:34:23 +0500

    Update SampleERC721.sol

--- SampleERC721.sol
+++ SampleERC721.sol
@@ -3,83 +3,61 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFT is ERC721Enumerable, Ownable {
-  using Strings for uint256;
+    using Strings for uint256;
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdCounter;
+    string public baseURI;
+    string public baseExtension = ".json";
+    string public contractURI;
 
-  string public baseURI;
-  string public baseExtension = ".json";
-  string public contractURI;
-  uint256 public maxSupply;
-  
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    string memory _initBaseURI,
-    uint256 _maxSupply
-  ) ERC721(_name, _symbol) {
-    setBaseURI(_initBaseURI);
-    maxSupply = _maxSupply;
-  }
-
-  // internal
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
-  }
-
-  // public
-  function mint(uint256 _mintAmount) public onlyOwner {
-
-    uint256 supply = totalSupply();
-    require(_mintAmount > 0, "need to mint at least 1 NFT");
-    require(supply + _mintAmount <= maxSupply, "max NFT limit exceeded");
-
-    for (uint256 i = 1; i <= _mintAmount; i++) {
-      _safeMint(msg.sender, supply + i);
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _initBaseURI
+    ) ERC721(_name, _symbol) {
+        setBaseURI(_initBaseURI);
     }
-  }
-  
-  function walletOfOwner(address _owner)
-    public
-    view
-    returns (uint256[] memory)
-  {
-    uint256 ownerTokenCount = balanceOf(_owner);
-    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-    for (uint256 i; i < ownerTokenCount; i++) {
-      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
     }
-    return tokenIds;
-  }
 
-  function tokenURI(uint256 tokenId)
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
-    
-    string memory currentBaseURI = _baseURI();
-    return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
-        : "";
-  }
+    // function returns URI of a given tokenId
+    function tokenURI(
+        uint256 tokenId
+    ) public view virtual override returns (string memory) {
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
+    }
 
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
-  }
+    // set base uri for your metadata
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
 
-  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-    baseExtension = _newBaseExtension;
-  }
-  
-   // Function Sets Contract Metadata for Collection Store Front.
-   function setContractURI(string calldata _contractURI) public onlyOwner {
-        contractURI = _contractURI;
+    // function sets baseExtension eg. json
+    function setBaseExtension(
+        string memory _newBaseExtension
+    ) public onlyOwner {
+        baseExtension = _newBaseExtension;
+    }
+
+    // internal function returns base uri
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
     }
 }
