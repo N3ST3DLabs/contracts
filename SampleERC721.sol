a4bbd53962bd70772fde67596756ffa539d91b27
N3ST3D Labs <134204495+N3ST3DLabs@users.noreply.github.com>
2023-12-16 18:50:26 +0500

    Update SampleERC721.sol

--- SampleERC721.sol
+++ SampleERC721.sol
@@ -1,17 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.6;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract NFT is ERC721Enumerable, Ownable {
+contract StonerPunkzOnMatic is ERC721Enumerable, Ownable {
     using Strings for uint256;
-    using Counters for Counters.Counter;
-    Counters.Counter private _tokenIdCounter;
-    string public baseURI;
-    string public baseExtension = ".json";
-    string public contractURI;
+    string public baseURI; // baseUri for all tokenIds
+    string public baseExtension = ".json"; // extension for baseURI
 
     constructor(
         string memory _name,
@@ -21,43 +17,49 @@ contract NFT is ERC721Enumerable, Ownable {
         setBaseURI(_initBaseURI);
     }
 
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+    // Mint function to be called by onlyOwner
+    function mint(uint256 _mintAmount) public onlyOwner {
+        uint256 supply = totalSupply();
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(msg.sender, supply + i);
+        }
     }
 
-    // function returns URI of a given tokenId
+    // Function return tokenIds metadata URI
+    // retruns tokenId uri used by marketed places api to fetch metadata
     function tokenURI(
-        uint256 tokenId
+        uint256 _tokenId
     ) public view virtual override returns (string memory) {
+        require(
+            _exists(_tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
         string memory currentBaseURI = _baseURI();
         return
             bytes(currentBaseURI).length > 0
                 ? string(
                     abi.encodePacked(
                         currentBaseURI,
-                        tokenId.toString(),
+                        _tokenId.toString(),
                         baseExtension
                     )
                 )
                 : "";
     }
 
-    // set base uri for your metadata
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
     }
 
-    // function sets baseExtension eg. json
     function setBaseExtension(
         string memory _newBaseExtension
     ) public onlyOwner {
         baseExtension = _newBaseExtension;
-    }
-
-    // internal function returns base uri
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseURI;
     }
 }
